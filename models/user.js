b920a11345f996114f884900cd23c4fb9ed86d8b
Mansi1236 <radhikey1236@gmail.com>
2023-03-01 21:20:10 +0530

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,67 @@
+const {createHmac , randomBytes} = require('crypto');
+
+
+const {Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+{
+fullname: {
+type: String,
+required: true,
+unique: true,
+},
+salt : {
+    type: String,
+    required : true,
+
+
+},
+
+
+password: {
+type: String,
+required : true,
+
+},
+profileImageURL: {
+type: String,
+default: "images/default.png",
+
+}, 
+role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+}
+
+
+}, 
+{
+     timestamps: true
+});
+
+userSchema.pre('save',  function (next)  {
+    const user = this; // here 'this' is pointing to the user
+
+if (!user.isModified('password')) return;
+
+
+const salt = randomBytes((16).toString());
+const hashedPassword = createHmac('sha256', salt).update(user.password).digest('hex');
+
+this.salt -salt;
+this.password = hashedPassword;
+
+next();
+
+
+
+
+
+
+});
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
